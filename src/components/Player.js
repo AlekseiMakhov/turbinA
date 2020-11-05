9f36eb08d2f922bf9f2c747f68d1f05a05eaa359
AlekseiMakhov <66823049+AlekseiMakhov@users.noreply.github.com>
2020-11-05 16:59:39 +0300

    dev: finish markup

--- src/components/Player.js
+++ src/components/Player.js
@@ -22,7 +22,7 @@ function Player({ isMobileMode }) {
             <div className={`player ${isExpanded ? 'player_expanded' : ''}`}>
                 <button type='button' className='button button_type_play'></button>
                 <h3 className='player__text'>Палево - Шаровары Вячеслава</h3>
-                <p className='player__text player__text_type_time'>3:22</p>
+                <p className='player__text player__text_type_time'>5:22</p>
                 {(isExpanded || isMobileMode) && <button id='switch' type='button' className='button button_type_switch' onClick={handleSwitchButtonClick}>{isSongTextOpen ? 'Релизы' : 'Текст песни' }</button>}
                 <button type='button' id='expand' className={`button ${isExpanded ? 'button_type_collapse' : 'button_type_expand'}`} onClick={handleExpandButtonClick}></button>
                 <div className='slider'>
